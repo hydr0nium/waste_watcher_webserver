1d4edf164902c9e067d84b58978830eb8c9dd1eb
Kiran <s8kigrae@uni-saarland.de>
2024-07-05 18:08:26 +0200

    removed auth

--- waste_watcher/waste_watcher/tests.py
+++ waste_watcher/waste_watcher/tests.py
@@ -18,53 +18,47 @@ class UserModelTest(TestCase):
 
 class ServerTest(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super(ServerTest, cls).setUpClass()
-        with open("password.txt", "r") as f:
-            cls.password = f.readline().strip()
-
     def test_scoreboard(self):
         c = Client()
-        res = c.get(f"/scoreboard?pass={ServerTest.password}")
+        res = c.get(f"/scoreboard")
         res = res.content.decode("utf-8").replace("\n", "").replace(" ", "")
-        self.assertIn("<body></body>", res)
+        self.assertIn("progress-bar", res)
 
     def test_useradd(self):
         c = Client()
-        res = c.get(f"/add_user?id=4&username=Testuser&pass={ServerTest.password}")
+        res = c.get(f"/add_user?id=4&username=Testuser")
         self.assertIn("User created", res.content.decode("utf-8"))
 
     def test_commit(self):
         c = Client()
-        res = c.get(f"/add_user?id=4&username=Testuser&pass={ServerTest.password}")
-        res = c.get(f"/commit?id=4&pass={ServerTest.password}")
+        res = c.get(f"/add_user?id=4&username=Testuser")
+        res = c.get(f"/commit?id=4")
         self.assertIn("Score of User", res.content.decode("utf-8"))
 
     def test_set_fill(self):
         c = Client()
-        res = c.get(f"/set_fill_amount?amount=30&pass={ServerTest.password}")
+        res = c.get(f"/set_fill_amount?amount=30")
         self.assertIn("Updated Trashcan amount", res.content.decode("utf-8"))
 
     def test_max_fill(self):
         c = Client()
-        res = c.get(f"/set_max_amount?amount=30&pass={ServerTest.password}")
+        res = c.get(f"/set_max_amount?amount=30")
         self.assertIn("Updated max Trashcan amount", res.content.decode("utf-8"))
 
     def test_resetdb(self):
         c = Client()
-        res = c.get(f"/add_user?id=4&username=Testuser&pass={ServerTest.password}")
-        res = c.get(f"/commit?id=4&pass={ServerTest.password}")
-        res = c.get(f"/reset?pass={ServerTest.password}")
+        res = c.get(f"/add_user?id=4&username=Testuser")
+        res = c.get(f"/commit?id=4")
+        res = c.get(f"/reset")
         self.assertIn("The database has been cleared", res.content.decode())
-        res = c.get(f"/scoreboard?pass={ServerTest.password}")
+        res = c.get(f"/scoreboard")
         res = res.content.decode("utf-8").replace("\n", "").replace(" ", "")
-        self.assertIn("<body></body>", res)
+        self.assertNotIn("Testuser", res)
 
     def test_user_already_in_db(self):
         c = Client()
-        res = c.get(f"/add_user?id=4&username=Testuser&pass={ServerTest.password}")
-        res = c.get(f"/add_user?id=4&username=Testuser2&pass={ServerTest.password}")
+        res = c.get(f"/add_user?id=4&username=Testuser")
+        res = c.get(f"/add_user?id=4&username=Testuser2")
         self.assertIn("User already exists", res.content.decode("utf-8"))
 
         
